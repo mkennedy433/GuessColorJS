3e164a023da99a933d2ce5f501997a512d596bfd
morganpk00 <morgan.kennedy433@west-mec.org>
2016-11-15 13:21:53 -0700

    Add files via upload

---
+++ jsgame.js
@@ -0,0 +1,47 @@
+/**
+ * Created by morgan.kennedy433 on 11/1/16.
+ */
+
+//make new function to slec random color
+
+var colors = ["BLUE", "CYAN", "GOLD", "GRAY", "GREEN", "MAGENTA", "ORANGE", "RED", "WHITE", "YELLOW"];
+var random = Math.floor(Math.random() * 9);
+var randcolor = colors [random];
+var clicks = 0;
+
+
+function game () {
+
+    var guess = document.getElementById('guess').value.toUpperCase();
+
+    clicks += 1;
+    document.getElementById("button").value = clicks;
+
+
+    try {
+
+        if (guess == randcolor) {
+            alert("Congratulations! You have guessed the color!" +
+                " It took you " + clicks + " guesses to finish the game!");
+            document.body.style.backgroundColor = randcolor;
+
+        }
+        else if (guess > randcolor) {
+            alert("Your guess is alphabetically lower than the color chosen.");
+        }
+
+        else if (guess < randcolor) {
+            alert("Your guess is alphabetically higher than the color chosen.");
+
+        }
+
+    }
+
+    catch (err) {
+            alert ( "This color is not an option! Please choose a valid color.");
+    }
+
+}
+
+
+
